1606b3c4874f00125d3ac0ded8756896300a451c
joaovidalsenai <joao.vidal@ba.estudante.senai.br>
2025-05-09 17:54:58 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,5 +1,24 @@
+const regex = /^[a-zA-Z0-9]+@[a-zA-Z0-9]+(\.[a-zA-Z]+)+$/;
+
 function enviarMensagem(event) {
     event.preventDefault();
     let nome = document.getElementById("nome").value.toString().trim().split(" ")[0]
-    alert(`Obrigado por usar nossos serviços ${nome}! Sua mensagem foi recebida e será analisada.`)
+
+    if (validarEmail(true)) {
+        alert(`Obrigado por usar nossos serviços ${nome}! Sua mensagem foi recebida e será analisada.`)
+    }
+    else {
+        alert(`Insira um email valido, ex: exemplo@dominio.com`)
+    }
+}
+
+function validarEmail(rtr) {
+    test = regex.test(document.getElementById("email").value.toString())
+    if (rtr) {
+        return test
+    } else {
+        if (!test) {
+            alert(`Insira um email valido, ex: exemplo@dominio.com`)
+        }
+    }
 }
