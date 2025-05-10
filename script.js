10b211a02244f5c88bead50c390627d5cd9f14a8
joaovidalsenai <joao.vidal@ba.estudante.senai.br>
2025-05-09 22:01:31 -0300

    Melhorias de responsividade e javascript

--- script.js
+++ script.js
@@ -1,24 +1,38 @@
-const regex = /^[a-zA-Z0-9]+@[a-zA-Z0-9]+(\.[a-zA-Z]+)+$/;
+const emailformat = /^(?!\.)(?!.*\.{2})[a-zA-Z0-9]+(?:\.[a-zA-Z0-9]+)*@(?:[a-zA-Z0-9]+\.)+[a-zA-Z]{2,}$/
+const nameformat = /^[A-Za-zÀ-ÖØ-öø-ÿÇç]+(?:\s+[A-Za-zÀ-ÖØ-öø-ÿÇç]+)+$/
+
 
 function enviarMensagem(event) {
-    event.preventDefault();
-    let nome = document.getElementById("nome").value.toString().trim().split(" ")[0]
+    event.preventDefault(); 
 
-    if (validarEmail(true)) {
-        alert(`Obrigado por usar nossos serviços ${nome}! Sua mensagem foi recebida e será analisada.`)
+    if (validarNome(1)) {
+        if (validarEmail(1)) {
+            let nome = document.getElementById("nome").value.toString().trim().split(" ")[0]
+            alert(`Obrigado por usar nossos serviços ${nome}! Sua mensagem foi recebida e será analisada.`)
+        }
+        else alert(`Insira um email valido (ex: exemplo@dominio.com)`)
     }
-    else {
-        alert(`Insira um email valido, ex: exemplo@dominio.com`)
-    }
+    else alert(`Insira um nome valido (ex: Nome Sobrenome)`)
 }
 
-function validarEmail(rtr) {
-    test = regex.test(document.getElementById("email").value.toString())
-    if (rtr) {
+function validarEmail(e) {
+    test = emailformat.test(document.getElementById("email").value.toString())
+    if (e) {
         return test
     } else {
         if (!test) {
-            alert(`Insira um email valido, ex: exemplo@dominio.com`)
+            alert(`Insira um email valido (ex: exemplo@dominio.com)`)
+        }
+    }
+}
+
+function validarNome(e) {
+    test = nameformat.test(document.getElementById("nome").value.toString())
+    if (e) {
+        return test
+    } else {
+        if (!test) {
+            alert(`Insira um nome valido (ex: Nome Sobrenome)`)
         }
     }
 }
