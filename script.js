c35b897ad46886044c829139b98ccc85d25215b0
joaovidalsenai <joao.vidal@ba.estudante.senai.br>
2025-05-10 09:30:45 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -36,22 +36,3 @@ function validarNome(e) {
         }
     }
 }
-
-document.addEventListener('DOMContentLoaded', function() {
-    document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-        anchor.addEventListener('click', function(e) {
-            e.preventDefault();
-            
-            const targetId = this.getAttribute('href');
-            if (targetId === '#') return;
-            
-            const targetElement = document.querySelector(targetId);
-            if (targetElement) {
-                window.scrollTo({
-                    top: targetElement.offsetTop - 0,
-                    behavior: 'smooth'
-                });
-            }
-        });
-    });
-});
